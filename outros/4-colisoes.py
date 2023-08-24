cb009fb3bb1214845b0e5fb871e1b1bc0cfbd30e
Gabriella-Aquino <gabyyaquino8@gmail.com>
2023-08-24 20:07:14 -0300

    3-4 atualizações

--- outros/4-colisoes.py
+++ outros/4-colisoes.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+from random import randint
 
 pygame.init()
 
@@ -8,10 +9,14 @@ largura = 400
 altura = 350
 x = (largura - 25)/2
 y = (altura - 30)/2
+
+x_azul = randint(40, 360) 
+y_azul = randint(50, 300)
+
 relogio = pygame.time.Clock()
 
 tela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption('Interação com o teclado')
+pygame.display.set_caption('Colisão')
 
 while True:
     relogio.tick(30)
@@ -20,15 +25,6 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-        '''if event.type == KEYDOWN:
-            if event.key == K_a:
-                x += -20
-            if event.key == K_d:
-                x += 20
-            if event.key == K_w:
-                y += -20
-            if event.key == K_s:
-                y += 20'''
                                       #   para as setas
     if pygame.key.get_pressed()[K_a]: # K_LEFT
         x += -10
@@ -39,7 +35,11 @@ while True:
     if pygame.key.get_pressed()[K_s]: # K_DOWN
         y += 10
 
-    pygame.draw.rect(tela, (200, 0, 0), (x, y, 25, 30))
-  
+    rect_vermelho = pygame.draw.rect(tela, (200, 0, 0), (x, y, 25, 30))
+    rect_azul = pygame.draw.rect(tela, (0,0,200), (x_azul, y_azul, 25, 30))
+
+    if rect_vermelho.colliderect(rect_azul):
+        x_azul = randint(40, 360) 
+        y_azul = randint(50, 300)
 
     pygame.display.update()
